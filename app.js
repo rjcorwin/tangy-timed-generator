b86223d81b2b7f64dbbac8d15a59694d01bfaa97
R.J. Steinert <rj@rjsteinert.com>
2017-10-31 01:26:23 -0400

    Incorporate tangy-timed with new improvements

--- app.js
+++ app.js
@@ -1,59 +1,55 @@
 
-function generate(text, name, numberOfColumns) {
+function generateFlat(text, name, numberOfColumns) {
 
   let fragments = text.split(' ')
   fragments = fragments.filter((fragment) => fragment.trim() !== '')
 
-  let numberOfRows = Math.ceil(fragments.length / numberOfColumns) 
+  let tangyTimed = document.createElement('tangy-timed')
+  tangyTimed.setAttribute('columns', numberOfColumns)
+  tangyTimed.setAttribute('id', name)
 
-  let table = document.createElement('div')
-  
-  for (var i=0; i < numberOfRows; i++) {
-    let row = document.createElement('div')
-    row.className = 'row'
-    for (var x=0; x < numberOfColumns; x++) {
-      let column = document.createElement('div')
-      column.className = 'column'
-      row.appendChild(column)
-    }
-    table.appendChild(row)
-  }
-
-   i = 0
-  let tr = document.createElement('tr')
-
-  rowPos = 0
-  columnPos = 0
+  let i = 0
   for (let fragment of fragments) {
     i++
-    let tr = table.childNodes[rowPos]
-    let td = tr.childNodes[columnPos]
-    let input = document.createElement('tangy-toggle-button')
+    let input = document.createElement('input')
     input.setAttribute('name', `${name}-${i}`)
-    input.setAttribute('value', '')
-    input.innerHTML = fragment
-    td.appendChild(input)
-    columnPos++
-    if (columnPos + 1 > numberOfColumns) {
-      columnPos = 0
-      rowPos++
-    }
+    input.setAttribute('value', 'on')
+    input.setAttribute('type', 'hidden')
+    input.setAttribute('placeholder', fragment)
+    tangyTimed.appendChild(input)
   }
+  // Set up calculated variables.
+  [
+    '_last_selected_name', 
+    '_last_selected_nth', 
+    '_time_remaining', 
+    '_time_spent', 
+    '_items_per_minute', 
+    '_number_of_items_marked'
+  ].forEach((variable) => {
+    let input = document.createElement('input')
+    input.setAttribute('name', `${name}${variable}`)
+    input.setAttribute('type', 'hidden')
+    tangyTimed.appendChild(input)
+  })
+
   window.demo.innerHTML = ''
-  window.demo.appendChild(table)
+  window.demo.appendChild(tangyTimed)
+
   let container = document.createElement('div')
-  container.appendChild(table.cloneNode(true))
+  container.appendChild(tangyTimed.cloneNode(true))
   return container.innerHTML
 }
 
+
 window.go.addEventListener('click', () => {
-  window.output.value = generate(
+  window.output.value = generateFlat(
     window.customText.value, 
     window.customName.value, 
     parseInt(window.numberOfColumns.value)
   )
 })
-window.output.value = generate(
+window.output.value = generateFlat(
   window.customText.value, 
   window.customName.value, 
   parseInt(window.numberOfColumns.value)
