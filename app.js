913015e56f007cba8db8d4400d2290d901486209
R.J. Steinert <rj@rjsteinert.com>
2017-10-30 21:42:56 -0400

    Grid generating

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
 
-function generate(text, name) {
+function generate(text, name, numberOfColumns) {
+
   let fragments = text.split(' ')
   fragments = fragments.filter((fragment) => fragment.trim() !== '')
-  let container = document.createElement('div')
-  let i = 0
+
+  let numberOfRows = Math.ceil(fragments.length / numberOfColumns) 
+
+  let table = document.createElement('table')
+  
+  for (var i=0; i < numberOfRows; i++) {
+		table.appendChild(document.createElement('tr'))
+	}
+	table.childNodes.forEach((row) => {
+		for (i=0; i < numberOfColumns; i++) {
+      row.appendChild(document.createElement('td'))
+		}
+	})
+
+ 	i = 0
+  let tr = document.createElement('tr')
+
+  rowPos = 0
+  columnPos = 0
   for (let fragment of fragments) {
-    let input = document.createElement('option')
     i++
+		let tr = table.childNodes[rowPos]
+		let td = tr.childNodes[columnPos]
+    let input = document.createElement('tangy-toggle-button')
     input.setAttribute('name', `${name}-${i}`)
     input.setAttribute('value', '')
     input.innerHTML = fragment
-    container.appendChild(input)
+    td.appendChild(input)
+		columnPos++
+		if (columnPos + 1 > numberOfColumns) {
+			columnPos = 0
+			rowPos++
+		}
   }
+  window.demo.innerHTML = ''
+  window.demo.appendChild(table)
+  let container = document.createElement('div')
+  container.appendChild(table.cloneNode(true))
   return container.innerHTML
 }
 
-window.go.addEventListener('click', () => {
-  window.output.value = generate(window.customText.value, window.customName.value)
-})
+//window.go.addEventListener('click', () => {
+  window.output.value = generate(
+		window.customText.value, 
+		window.customName.value, 
+		parseInt(window.numberOfColumns.value)
+	)
+//})
+
 
 
